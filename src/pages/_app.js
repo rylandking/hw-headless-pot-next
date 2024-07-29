c8d1ef243837fb25cae75505dade31d1d64e16a6
Subasri1204 <168529469+Subasri1204@users.noreply.github.com>
2024-07-29 18:35:16 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,16 @@
 import '../utils/css/App.css';
+import '../utils/css/Breadcrumb.css';
+import '../utils/css/CardSection3.css';
+import '../utils/css/dropdownmodel.css';
+import '../utils/css/home.css';
+import '../utils/css/NotFound.css';
+import '../utils/css/index.css';
+import '../utils/css/pdp.css';
+import '../utils/css/ProductSection.css';
+import '../utils/css/searchBarB.css';
+import '../utils/css/searchBarW.css';
+import '../utils/css/soldToDropdown.css';
+import '../utils/css/Testingdrop.css';
 import Layout from './Layout.js';
 
 function MyApp({ Component, pageProps }) {
