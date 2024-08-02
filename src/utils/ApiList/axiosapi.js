7defb17a136698ed81017078d86106ebb5e10e9b
Subasri1204 <168529469+Subasri1204@users.noreply.github.com>
2024-08-02 16:00:50 +0530

    Update axiosapi.js

--- src/utils/ApiList/axiosapi.js
+++ src/utils/ApiList/axiosapi.js
@@ -16,7 +16,11 @@ const refreshApi = "http://localhost:3002/pif/api/session/refresh?appId=239";
 const userApi = "http://localhost:3002/pif/api/user/v1/get-user?appId=239";
 const statusApi = "http://localhost:3002/pif/api/status/v1/get-status?appId=239";
 
+const productdetailApi = "https://dummyjson.com/c/c798-bc76-46d3-aa9e";
+const productpriceApi = "https://dummyjson.com/c/3874-ddca-4cd1-814b";
+const atpApi = "https://dummyjson.com/c/2726-894c-4fa5-ba7b";
 
-export { contactApi, detailsApi, toolsApi, refreshApi, userApi, statusApi };
+
+export { contactApi, detailsApi, toolsApi, refreshApi, userApi, statusApi, productdetailApi, productpriceApi, atpApi };
 
 
