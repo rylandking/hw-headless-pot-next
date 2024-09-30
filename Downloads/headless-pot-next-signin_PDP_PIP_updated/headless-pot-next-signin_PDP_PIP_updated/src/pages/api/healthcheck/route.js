e3cdec4be6a72041f4a94310f02f05bdf6f01967
PunitGitHub88 <Honeywellpublic@21>
2024-09-30 13:13:17 -0400

    Dir Cleanup

--- Downloads/headless-pot-next-signin_PDP_PIP_updated/headless-pot-next-signin_PDP_PIP_updated/src/pages/api/healthcheck/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET(req) {   
-    return new Response(JSON.stringify({ uptime: process.uptime(), status: 'success' }), {
-        status: 200,        
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-}
